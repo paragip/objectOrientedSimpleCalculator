6ceb59b31de555f2813814b70d4fbc913225b1e9
paragip <pal.paragi@gmail.com>
2022-06-12 12:44:34 +0200

    SimpleCalculator.java - calculating results from 2 numbers (private variables in class SimpleCalculator). 8 methods were needed. checking in Main.java.

---
+++ src/pal/paragi/Main.java
@@ -0,0 +1,21 @@
+package pal.paragi;
+
+public class Main { // ellenőrzéshez, ez nem része a feladatnak
+
+    public static void main(String[] args) {
+
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+
+        System.out.println("add = " + calculator.getAdditionResult());
+        System.out.println("subtract = " + calculator.getSubtractionResult());
+
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply = " + calculator.getMultiplicationResult());
+        System.out.println("divide = " + calculator.getDivisionResult());
+
+    }
+
+}
